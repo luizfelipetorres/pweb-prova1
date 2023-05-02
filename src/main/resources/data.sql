99ed53fd93890ad13147606b52cdbaef1d446670
luizfelipetorres <fellipe116@gmail.com>
2023-05-02 18:52:33 -0300

    remover atuações sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,6 +6,3 @@ INSERT INTO projetos(nome, custo, categoria) VALUES('Nova Moradia', 27000, 'PRIV
 INSERT INTO projetos(nome, custo, categoria) VALUES('Lago artificial', 32000, 'PARCERIA');
 INSERT INTO projetos(nome, custo, categoria) VALUES('Quadra de Futebol', 10000, 'PUBLICO');
 
-INSERT INTO atuacoes(engenheiro_id, projeto_id, duracao) VALUES(1, 3, 150);
-INSERT INTO atuacoes(engenheiro_id, projeto_id, duracao) VALUES(2, 1, 270);
-INSERT INTO atuacoes(engenheiro_id, projeto_id, duracao) VALUES(3, 2, 400);
